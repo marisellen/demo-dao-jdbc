65218e1cf262032e9ec15b15a6cc669145de6369
marianaselene <mariana02301@gmail.com>
2026-08-10 09:06:15 -0300

    DepartmentDao ad SellerDao interfaces

---
+++ src/Model/DAO/DepartmentDao.java
@@ -0,0 +1,17 @@
+package Model.DAO;
+
+import entities.Department;
+
+import java.util.List;
+
+public interface DepartmentDao {
+    void insert(Department obj);
+
+    void update(Department obj);
+
+    void deleteById(Integer id);
+
+    Department findById(Integer id);
+
+    List<Department> findAll();
+}

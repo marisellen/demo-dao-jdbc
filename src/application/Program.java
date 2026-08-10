6f66be6a39d53bed45944ae550961e98427dc531
marianaselene <mariana02301@gmail.com>
2026-08-10 08:53:31 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,15 @@
 package application;
 
 import entities.Department;
+import entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
         Department obj = new Department(1,"Books");
+        Seller seller = new Seller(1,"Maria","maria@gmail.com", new Date(), 3000.0, obj);
         System.out.println(obj);
+        System.out.println(seller);
     }
 }

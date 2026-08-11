f63e869c5126a7d292797efb2a7335c29fb8aa0c
marianaselene <mariana02301@gmail.com>
2026-08-11 15:57:10 -0300

    reusing instantiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,12 +9,11 @@ import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
-        Department obj = new Department(1,"Books");
-        Seller seller = new Seller(1,"Maria","maria@gmail.com", new Date(), 3000.0, obj);
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
-        System.out.println(obj);
+        System.out.println("--- Teste 1 ---");
+        Seller seller = sellerDao.findById(1);
+
         System.out.println(seller);
     }
 }

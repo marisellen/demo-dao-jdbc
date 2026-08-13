67975df993e2302e438b22fa488facffc6856455
marianaselene <mariana02301@gmail.com>
2026-08-12 23:02:18 -0300

    DepartmentDao

--- src/Model/DAO/Impl/DaoFactory.java
+++ src/Model/DAO/Impl/DaoFactory.java
@@ -7,4 +7,8 @@ public class DaoFactory {
     public static SellerDao createSellerDao() {
         return new SellerDaoJDBC(DB.getConnection());
     }
+
+    public static DepartmentDaoJDBC createDepartmentDao() {
+        return new DepartmentDaoJDBC(DB.getConnection());
+    }
 }
